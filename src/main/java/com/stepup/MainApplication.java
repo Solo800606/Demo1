6afdd3e3fa00d34233b23ca5a0c1619921f116aa
Solo800606 <61974103+Solo800606@users.noreply.github.com>
2024-06-27 21:34:43 +0300

    Добавлен метод doSomething()

--- src/main/java/com/stepup/MainApplication.java
+++ src/main/java/com/stepup/MainApplication.java
@@ -7,7 +7,7 @@ public class MainApplication {
         System.out.println("111");
         System.out.println("222");
         System.out.println(-----);
-
+        doSomething();
         System.out.println("333");
         System.out.println("000");
     }
