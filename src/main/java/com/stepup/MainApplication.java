53363842142996476393bec5ad38c13e4a0db639
Solo800606 <61974103+Solo800606@users.noreply.github.com>
2024-06-27 20:32:35 +0300

    Доработка, добавлены принты

--- src/main/java/com/stepup/MainApplication.java
+++ src/main/java/com/stepup/MainApplication.java
@@ -3,5 +3,7 @@ package com.stepup;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("111111");
+        System.out.println("SSASASAS");
     }
 }
