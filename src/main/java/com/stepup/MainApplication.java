26252394a670724e9090cff42f2c9938843c0b65
Solo800606 <61974103+Solo800606@users.noreply.github.com>
2024-06-27 20:52:43 +0300

    Изменена основная логика Добавлена Коробка

--- src/main/java/com/stepup/MainApplication.java
+++ src/main/java/com/stepup/MainApplication.java
@@ -4,6 +4,13 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("11111");
-        System.out.println("SSASSAS");
+        System.out.println("222");
+
+        System.out.println("333");
+        System.out.println("4444");
+    }
+
+    public static void doSomething() {
+        System.out.println("SOLO");
     }
 }
