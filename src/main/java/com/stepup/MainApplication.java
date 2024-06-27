648d0307f1da0dd735f2e04cbe066fa500248f9b
Solo800606 <61974103+Solo800606@users.noreply.github.com>
2024-06-27 21:32:48 +0300

    Добавлено сообщение Привет Мир!

--- src/main/java/com/stepup/MainApplication.java
+++ src/main/java/com/stepup/MainApplication.java
@@ -13,8 +13,6 @@ public class MainApplication {
     }
 
     public static void doSomething() {
-        System.out.println(1);
-        System.out.println(1);
-        System.out.println(2);
+        System.out.println("Hello World!");
     }
 }
